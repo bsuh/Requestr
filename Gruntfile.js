287a0f32f21757ed507b02d6e924af03cd99bc01
José Antonio Márquez <jam@tradeshift.com>
2013-12-03 16:56:34 -0800

    Adding hook with Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,11 @@
 /*jshint node:true*/
 module.exports = function(grunt) {
-  //
+
   grunt.initConfig({
-    //
+
     pkg: grunt.file.readJSON('package.json'),
-    //
+    
+    // JSHint with options.
     jshint: {
       options: {
         jshintrc: '.jshintrc',
@@ -13,21 +14,26 @@ module.exports = function(grunt) {
         '.'
       ]
     },
-    //
+    
+    // Command-line tasks.
     shell: {
       fixjsstyle: {
         options: {
           stdout: true
         },
+        // Closure tools styling.
         command: 'fixjsstyle lib/requestr.js specs/requestr_test.js'
       },
       gjslint: {
         options: {
           stdout: true
         },
+        // Closure tools linting.
         command: 'gjslint lib/requestr.js specs/requestr_test.js'
       }
     },
+    
+    // Minification.
     uglify: {
       my_target: {
         files: {
@@ -35,19 +41,28 @@ module.exports = function(grunt) {
         }
       }
     },
-    //
+    
+    // Unit tests.
     jasmine: {
       src: 'lib/requestr.js',
       options: {
         specs: 'specs/requestr_test.js'
       }
+    },
+    
+    // Git hooks with Grunt.
+    githooks: {
+      all: {
+        'pre-commit': 'default'
+      }
     }
   });
-  //
+
   grunt.loadNpmTasks('grunt-shell');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-jasmine');
-  //
+  grunt.loadNpmTasks('grunt-githooks');
+
   grunt.registerTask('default', ['shell', 'jshint', 'jasmine', 'uglify']);
 };
